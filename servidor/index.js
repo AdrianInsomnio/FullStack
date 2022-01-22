accd417d625cb49c164deb1f7d16f9fdb0879dc2
Adrian Rodriguez <Appxims@gmail.com>
2022-01-22 19:00:55 -0300

    BK- FIX CON

--- servidor/index.js
+++ servidor/index.js
@@ -14,12 +14,12 @@ app.use(cors());
 require('dotenv').config();
 
 // db connection
-
+const env = process.env;
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'dev1234',
-  database: 'mysql_tutorial_db',
+  host: env.DB_HOST,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME
 });
 
 

84dd5393a0fa44303fde867bd40152ebc7b15dc2
Adrián Rodíguez <Appxims@gmail.com>
2022-01-28 08:46:24 -0300

    componente home-incompleto

--- cliente/src/components/home/index.js
+++ cliente/src/components/home/index.js
@@ -2,40 +2,63 @@ import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Info from './Info';
 import Axios from 'axios';
+import './home.css';
+import Card from './Card';
+
 
 const Home = (props) => {
-    const [ total, setTotal]= useState(0);
+    //const [ total, setTotal]= useState(0);
     const [list,setList]= useState([]);
+    const actualizar= true;
 
-    const baseUrl ='http://localhost:5000/user';
+    const baseUrl ='http://localhost:5000/expenses/all';
+    
+    const homeUrl ='http://localhost:5000/home';
+
+    const [data ,setData]  =useState({}); 
     
 
-    useEffect(() => {
+    const fetchdata = ()=>{ 
       Axios({
-        url: baseUrl,
+        url: homeUrl,
       })
         .then((response) => {
-          setList(response.data);
+          setList(response.data.top10);
+          setData(response.data);
+          console.log(response);
         })
         .catch((error) => {
           console.log(error);
         });
+    };
+
+    useEffect(() => {
+      fetchdata();
     }, []);
+    const { Total, Incomes , Outcome , top10 } = data;
 
-
-  return <div className="" style={ {textAlign:"center",background: "aqua"}}>
+  return (
+    <div className="" style={ {textAlign:"center"}}>
       <Header  title="Expenses traker" />
-      <Info total= { total} />
-      <ul>
-      {list.map((item) => (
-          <div key={item.id}>
-            <h3>{item.nombre}</h3>
-            <p>{item.pass}</p>
-          </div>
-        ))}
-      </ul>
+      {
+        //console.log(data.top10)
+      }
+      <Info mont= { Total} />
+      
+      <p> Entradas ${Incomes} - Salidas ${Outcome}  </p>
+      
+      <h3> Last 10 transacction </h3>
+      <div className='contenedor-top' > 
+        {
+          top10?.map( item =>
+            <Card key={item.idconcepto} item={item} />
+          )
+        } 
+      </div>
      
-  </div>;
+    </div>
+  )
+
 };
 
 export default Home;

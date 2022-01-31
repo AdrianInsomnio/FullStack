323b7f86b7f0eb738e41130b9a1991d86f1aa0df
Adrián Rodíguez <Appxims@gmail.com>
2022-01-31 14:48:32 -0300

    add Form component-routes in app.js and view page

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,12 +1,45 @@
 import './App.css';
 import Home from './components/home';
+import {
+  Routes,
+  Route,
+  Link,
+  Outlet
+} from "react-router-dom";
+import Views from './components/abm/Views';
+import Form from './components/abm/Form';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="home" element={<Home />} />
+          <Route path="view" element={<Views />} />
+          <Route path="form" element={<Form />} />
+        </Route>
+    </Routes>
     </div>
   );
+
+  
+function Layout() {
+  return (
+    <div>
+      <h1>Welcome to the app!</h1>
+      <nav>
+        <Link to="Home">Home</Link> |{" "}
+        <Link to="view">View</Link> |{" "}
+        <Link to="form">+ New Transaction</Link>
+      </nav>
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
 }
 
 export default App;
---
+++ cliente/src/components/abm/Form.js
@@ -0,0 +1,49 @@
+import axios, { Axios } from 'axios';
+import { Alert } from 'bootstrap';
+import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+
+const Form = (props) => {
+
+  const {Titulo="Nuevo" } = props;
+    /*
+  const [detalle,setDetalle]=useState("");
+  const[monto,setMonto]=useState("");
+  const [optionsState,setOption]= useState("");
+*/
+const { register, formState: { errors }, reset, handleSubmit } = useForm( {
+  defaultValues: {
+    concepto: '',
+    monto: '1',
+    tipo:'I'
+  }
+} );
+
+var urlPost= 'http://localhost:5000/expenses/create';
+const onSubmit = (data,e) => {
+  console.log(data);
+  const post =  axios.post(urlPost,data)
+    .then(response => alert("Data recive"))
+    reset();
+  }
+  return 
+  <div className='col-md-6'>
+    <h1>{Titulo}</h1>
+    <form className='vstack d-grid gap-3 m-2' onSubmit={handleSubmit(onSubmit)} onReset={reset} >
+      <label>Detail</label>
+      <input {...register("concepto", { required: true, maxLength: 50 })} />
+        {errors.concepto?.type === 'required' && "Detail info is required"}
+      <label >Amount</label>
+      <input type="number" {...register("monto", { required:true , min: 1})} />
+        {errors.monto?.type === 'required' && "Amount is required"}
+      <label>Select Income or outcome</label>
+      <select {...register("tipo")}>
+        <option value ="I">Income</option>
+        <option value ="O">Outcome</option>
+      </select>
+      {errors.tipo?.type === 'required' && "Select income is required"}
+      <input type="submit"/>
+    </form></div>;
+};
+
+export default Form;

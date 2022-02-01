7c843309b79a6669092881f362d0bfb44752a900
Adrián Rodíguez <Appxims@gmail.com>
2022-02-01 09:05:25 -0300

    add Views component

--- cliente/src/components/abm/Views.js
+++ cliente/src/components/abm/Views.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Views = () => {
-  return <div>View</div>;
+  const [list ,setList]= useState([]);
+
+  const baseUrl ='http://localhost:5000/expenses/all';
+
+    const fetchdata = ()=>{ 
+      axios({
+        url: baseUrl,
+      })
+        .then((response) => {
+          setList(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
+  function eliminar(id) {
+    alert("eliminar");
+  }
+  function actualizar(id){
+    alert("actualizar");
+  }
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+  
+
+  return <div className='container'>
+    <div className='' >
+      <h1>All data</h1>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Description</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Date</th>
+            <th scope="col">Type</th>
+            <th scope="col">Acction</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+            list?.map((item) => (
+                <tr key={item.idoperacion}>
+                    <td>{item.idoperacion}</td>
+                    <td>{item.concepto}</td>
+                    <td>{item.monto}</td>
+                    <td>{item.fecha}</td>
+                    <td>{ item.tipo == "I" ? <p className='bg-success'> Incomes</p> : <p className='bg-danger'>Outcome </p> }</td>
+                    <td><button onClick={actualizar}>update</button><button onClick={eliminar}>delete</button></td>
+                    <td/>
+                </tr>
+            ))
+        }
+              
+         
+        </tbody>
+      </table>
+    </div>
+  </div>;
 };
 
 export default Views;

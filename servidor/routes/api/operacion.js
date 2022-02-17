c877707b699468dadaf9e69fb66bce0a1e01a321
Adrián Rodíguez <Appxims@gmail.com>
2022-02-17 09:53:19 -0300

    put delete

--- servidor/routes/api/operacion.js
+++ servidor/routes/api/operacion.js
@@ -3,13 +3,29 @@ const { Operacion } = require('../../db');
 
 
 
-router.get('/',(req,res)=>{
-    res.send("funciona");
+router.get('/',async (req,res)=>{
+    const all =await Operacion.findAll();
+    res.json(all);
 });
 
 router.post('/create',async (req,res)=>{
     const oper = await Operacion.create(req.body);
     res.json(oper)
 })
+
+router.put('/update/:op_id',async (req,res)=>{
+    await Operacion.update( req.body,{
+        where: { id : req.params.op_id }
+    })
+    res.send({ msg: "Registro modificado"})
+});
+
+router.delete('/remove/:op_id', async (req,res)=>{
+    await Operacion.destroy({
+        where: { id: req.params.op_id}
+    });
+    res.json({success : "se ha eliminado la operacion"})
+});
+
 
 module.exports = router;

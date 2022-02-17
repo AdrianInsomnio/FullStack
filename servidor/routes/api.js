b60855ca662565690a72972e2926313e7b4b8e3b
Adrián Rodíguez <Appxims@gmail.com>
2022-02-17 14:04:33 -0300

    midleware con token

--- servidor/routes/api.js
+++ servidor/routes/api.js
@@ -2,9 +2,10 @@ const router =  require('express').Router();
 
 const apiOperacionRouter = require('./api/operacion');
 const apiUsuarioRouter = require('./api/usuario');
+const middleware = require('./api/middleware');
 
 router.use('/users',apiUsuarioRouter);
-router.use('/expenses',apiOperacionRouter);
+router.use('/expenses',middleware.checkToken ,apiOperacionRouter);
 
 
 module.exports = router;

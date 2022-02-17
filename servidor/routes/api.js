2c35e654ca75a2a85c86b1167ee5bc2b7a7dce26
Adrián Rodíguez <Appxims@gmail.com>
2022-02-17 09:23:38 -0300

    operacion post

--- servidor/routes/api.js
+++ servidor/routes/api.js
@@ -2,6 +2,6 @@ const router =  require('express').Router();
 
 const apiOperacionRouter = require('./api/operacion');
 
-router.use('/operacion',apiOperacionRouter);
+router.use('/expenses',apiOperacionRouter);
 
 module.exports = router;
--- servidor/routes/api/operacion.js
+++ servidor/routes/api/operacion.js
@@ -1,7 +1,15 @@
 const router =  require('express').Router();
+const { Operacion } = require('../../db');
+
+
 
 router.get('/',(req,res)=>{
     res.send("funciona");
 });
+
+router.post('/create',async (req,res)=>{
+    const oper = await Operacion.create(req.body);
+    res.json(oper)
+})
 
 module.exports = router;

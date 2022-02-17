20ec04ecb8f8f1d1716acf28a9a80441162c5e4e
Adrián Rodíguez <Appxims@gmail.com>
2022-02-17 14:04:18 -0300

    operaciones con validaciones

--- servidor/routes/api/operacion.js
+++ servidor/routes/api/operacion.js
@@ -1,5 +1,6 @@
 const router =  require('express').Router();
-const { Operacion } = require('../../db');
+const { Operacion,Usuario } = require('../../db');
+const {check,validationResult} = require('express-validator');
 
 
 
@@ -8,12 +9,34 @@ router.get('/',async (req,res)=>{
     res.json(all);
 });
 
-router.post('/create',async (req,res)=>{
+router.post('/create',[
+    //`concepto`, `monto`, `tipo`, `usuarioId`
+    check('concepto',"Description is require").exists().isLength({min:2}),
+    check('monto','Value must be a number').exists().isNumeric({ min: 0}),
+    check('tipo','Value must be only INCOME or OUTCOME').isIn(['INCOME', 'OUTCOME'])
+
+],async (req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(422).json({errors : errors.array()})
+    }
+
     const oper = await Operacion.create(req.body);
     res.json(oper)
 })
 
-router.put('/update/:op_id',async (req,res)=>{
+router.put('/update/:op_id',[
+    check('concepto',"Description is require").exists().isLength({min:2}),
+    check('monto','Value must be a number').exists().isNumeric({ min: 0}),
+    check('tipo','Value must be only INCOME or OUTCOME').isEmpty()
+
+],async (req,res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(422).json({errors : errors.array()})
+    }
+
     await Operacion.update( req.body,{
         where: { id : req.params.op_id }
     })

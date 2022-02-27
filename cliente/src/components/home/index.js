f2ec3fada11ee113dce5c2aa5b5ea4e225c1527d
Adrián Rodíguez <Appxims@gmail.com>
2022-02-27 14:57:45 -0300

    clean code

--- cliente/src/components/home/index.js
+++ cliente/src/components/home/index.js
@@ -1,31 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import Header from './Header';
 import Info from './Info';
 import Axios from 'axios';
 import './home.css';
 import Card from './Card';
 
-
 const Home = (props) => {
-    //const [ total, setTotal]= useState(0);
-    const [list,setList]= useState([]);
-    const actualizar= true;
-
-    const baseUrl ='http://localhost:5000/api/expenses/';
     
     const homeUrl ='http://localhost:5000/api/home';
 
     const [data ,setData]  =useState({}); 
     
-
     const fetchdata = ()=>{ 
       Axios({
         url: homeUrl,
       })
         .then((response) => {
-          setList(response.data.top10);
           setData(response.data);
-          console.log(response);
         })
         .catch((error) => {
           console.log(error);
@@ -39,23 +29,25 @@ const Home = (props) => {
 
   return (
     <div className="container" style={ {textAlign:"center"}}>
-      <Info mont= { Total} />     
-      <p> Total  Incomes ${Incomes} - Total Outcomes ${Outcome}  </p>
-      
-      <h3> Last 10 transacction </h3>
-      <div className='container'>
-      <div className='row row-cols-5 ' > 
-        {
-          //console.log(top10)
-        }
-        {
-          top10?.map( item =>
-            <Card key={item.idoperacion} item={item} />
-          )
-        } 
-      </div>
-      </div>
-     
+      <div>
+        <Info mont= { Total} />     
+        <p> Total  Incomes ${Incomes} - Total Outcomes ${Outcome}  </p>
+        
+        <h3> Last 10 transacction </h3>
+
+        <div className='container'>
+          <div className='mx-auto d-flex flex-wrap ' > 
+            {
+              top10?.map( (item,index) =>
+              <li className='d-flex w-30' key={index}>
+                <Card item={item} />
+              </li>
+                
+              )
+            } 
+            </div>
+          </div>
+        </div>     
     </div>
   )
 
